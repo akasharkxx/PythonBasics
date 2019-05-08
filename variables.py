82306a840fa6543d97d8636f1b7f7c614272c644
akasharkxx <akash.jaiswar41@gmail.com>
2019-05-08 07:03:00 +0530

    Basics of python
    Operations on Numbers
    Operations on string

---
+++ variables.py
@@ -0,0 +1,26 @@
+# variables
+# Basic Types
+# Operations on String
+# Operations on Numbers
+
+# Number and Strings
+number1 = 10
+string1 = "Hello"
+string2 = "C++ is great Programming language"
+number2 = 3
+# + - / * % ** //
+# print(number1+number2)
+# print(number1-number2)
+# print(number1*number2)
+# print(number1/number2)
+# print(number1%number2)
+# print(number1**number2)
+# print(number1//number2)
+
+print(string1 + " guys")
+print(len(string1))
+print(string2.find("C++"))
+print(string2.isalnum())
+print(string2.isdecimal())
+print(string2.replace("great","fantastic"))
+print(string2.split(" "))
